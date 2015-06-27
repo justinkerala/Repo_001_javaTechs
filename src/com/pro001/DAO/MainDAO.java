294e77cdfb6ce7cf34d2474f5c32de818f286541
justin maniyamprayil <justinkerala@yahoo.com>
2015-06-27 17:03:28 +0530

    JM

--- src/com/pro001/DAO/MainDAO.java
+++ src/com/pro001/DAO/MainDAO.java
@@ -3,18 +3,8 @@ package com.pro001.DAO;
 public class MainDAO {
 
 	public static void main(String[] args) {
-		System.out.println("haisdh");
-		System.out.println("haisdh");
-		System.out.println("haisdh");
-		System.out.println("haisdh");
 		
-		System.out.println("haisdh");
-		System.out.println("haisdh");
-		System.out.println("haisdh");
-		
-		
-		
-		System.out.println("haisdh");
+		System.out.println("starting");
 		
 		
 	}
